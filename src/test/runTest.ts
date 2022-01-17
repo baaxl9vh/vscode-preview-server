320b0ab14890f59b93680747ae2e6d1080adea80
jay <jay@imac.local>
2022-01-17 15:43:22 +0800

    ci: use cache for ci
    1. ignore language ts file's lint.
    2. fix package's description word.
    3. runTest use vscode ver: 1.63.2 for ci cache
    4. ci script use cache, now to test it.

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,4 +1,4 @@
-import { runTests } from '@vscode/test-electron';
+import { downloadAndUnzipVSCode, runTests } from '@vscode/test-electron';
 import * as path from 'path';
 
 async function main() {
@@ -9,7 +9,7 @@ async function main() {
 		const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
 		// https://az764295.vo.msecnd.net/insider/c6e58d126f6b5676242e32b34180b2a595f29b51/VSCode-darwin.zip
-		// const vscodeExecutablePath = await downloadAndUnzipVSCode('1.63.2');
+		const vscodeExecutablePath = await downloadAndUnzipVSCode('1.63.2');
 		// const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
 
     // // Use cp.spawn / cp.exec for custom setup
@@ -26,7 +26,7 @@ async function main() {
 
 		// Download VS Code, unzip it and run the integration test
 		// await runTests({ extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace] });
-		await runTests({ reuseMachineInstall: false, extensionDevelopmentPath, extensionTestsPath, launchArgs: [testWorkspace] });
+		await runTests({ reuseMachineInstall: false, extensionDevelopmentPath, extensionTestsPath, vscodeExecutablePath, launchArgs: [testWorkspace] });
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
